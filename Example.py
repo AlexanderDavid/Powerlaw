f147c23d6ae1a7ca98e306d93ceb3e3a5b4e8f6c
Alex Day <alexday135@gmail.com>
2021-02-24 17:48:27 -0500

    Update example to work with new code

--- Example.py
+++ Example.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Create the engine with a size of 50x50, time step of 0.005,
 # and max of 5000 frames
-engine = PySimulationEngine(50, 50, 0.005, 5000)
+engine = PySimulationEngine(50, 50, 0.033, 6000)
 
 # Set up the default parameters
 k = 1.5
@@ -40,14 +40,30 @@ for position, goal in zip(positions, goals):
                 m,
                 t0))
 
+
 # Add an Obstacle through the middle
-engine.addObstacle((0, 5), (5, 5))
+# engine.addObstacle((0, 5), (5, 5))
 
 
 # Loop the simulation
+i = 0
 while not engine.endSimulation():
+    # Calculatate goal velocity
+    for agent, goal in zip(agent_nums, goals):
+        np_goal = np.array(goal)
+        np_pos = np.array(engine.getAgentPosition(agent))
+        goal_vec = np_goal - np_pos
+        if np.linalg.norm(goal_vec) > prefspeed:
+            goal_vec = (goal_vec / np.linalg.norm(goal_vec)) * prefspeed
+
+        # Set the agents goal velocity
+        engine.setAgentPrefVelocity(agent_nums[-1], tuple(goal_vec))
+
     engine.updateVisualisation()
     engine.updateSimulation()
+    i += 1
+
+
 
 print("Final Positions")
 # Report the location of all agents
